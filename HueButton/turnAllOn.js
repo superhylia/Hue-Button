62a5a4854d4fc56d2502739d9f45a617dba269ab
superhylia <43157926+superhylia@users.noreply.github.com>
2019-11-05 19:03:30 -0600

    Update turnAllOn.js

--- HueButton/turnAllOn.js
+++ HueButton/turnAllOn.js
@@ -5,6 +5,8 @@
 
 var HOSTNAME = "192.168.0.101";
 var USERNAME = "82bf6d045f12856fa06cb642cbff0e";
+// Hostname is filled in with your Phillips Hue Bridge's IP address, which can be found at https://www.meethue.com/api/nupnp under the "internalipaddress"
+// Username is filled in with your Phillips Hue Bridge's ID, which can be found at https://www.meethue.com/api/nupnp under the "id"
 
 var Cylon = require("cylon");
 
@@ -15,25 +17,13 @@ Cylon.robot({
 
   devices: {
     bulb1: { driver: "hue-light", lightId: 1 },
-    bulb2: { driver: "hue-light", lightId: 2 },
-    bulb3: { driver: "hue-light", lightId: 3 },
-    bulb4: { driver: "hue-light", lightId: 4 },
-    bulb5: { driver: "hue-light", lightId: 5 },
-    bulb6: { driver: "hue-light", lightId: 6 },
-    bulb7: { driver: "hue-light", lightId: 7 }
   },
 
   work: function(my) {
     for (var d in my.devices) {
       my.devices[d].turnOn();
-      my.devices[d].brightness(75);
+      my.devices[d].brightness(100);
     }
-    my.bulb1.rgb(255,0,0); //red
-    my.bulb2.rgb(255,128,0); //orange
-    my.bulb3.rgb(255,255,0); //yellow
-    my.bulb4.rgb(0,255,0); //green
-    my.bulb5.rgb(0,0,255); //blue
-    my.bulb6.rgb(191,0,255); //purple
-    my.bulb7.rgb(255,0,191); //pink
+    my.bulb1.rgb(255,255,255); //white
   }
 }).start();
